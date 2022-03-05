7be2cbcc6f3c0e4778e9d328347630683b332b18
unknown <jshigoodies@gmail.com>
2022-03-05 17:36:41 -0600

    input loop

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 
 import voice_audio as va
+from colors import COLOR
 
+while True:
+    text = va.get_audio()
+    text = text.lower()
+    print(COLOR.RESET + "you said: ", text) # comment out
 
-text = va.get_audio()
-
-print("you said: ", text) # comment out
-
-va.check(text=text)
+    va.check(text=text)
 
 
--- voice_audio.py
+++ voice_audio.py
@@ -8,7 +8,7 @@ from colors import COLOR
 
 
 # initialize a global arary of strings. Such as greetings. Takes in all the "hello", "hi", "how are you", "howdy",
-# "greetings", "bonjour". 
+# "greetings", "bonjour".
 
 
 # make a method that checks the input text. Return a string of the audio file back. Use playsound to play the sound.
@@ -22,16 +22,20 @@ def get_audio():
         try:
             said = r.recognize_google(audio)
         except Exception as e:
-            print("Exception: " + str(e))
+            print(COLOR.RED + "*Qiqi could not hear you*")
 
     return said
 
 
 # check the text that would play the right audio back
 def check(text):
-    if "hello" in text:  # change this later
-        print(COLOR.RED + "Qiqi, I'm a jiangshi. Hm...? What else...")
-        playsound("audio/hello.mp3")
-    if "morning" in text:
-        print(COLOR.RED + "It's morning already? What should I... I'll check my notes.")
-        playsound("audio/good_morning.mp3")
+
+    # have a way to shut her up, maybe make a method for both shut up and un-shut up
+
+    if "chichi" in text: # you have to say her name
+        if "hello" in text:  # change this later so get an array of greetings
+            print(COLOR.RED + "Qiqi, I'm a jiangshi. Hm...? What else...")
+            playsound("audio/hello.mp3")
+        if "morning" in text:
+            print(COLOR.RED + "It's morning already? What should I... I'll check my notes.")
+            playsound("audio/good_morning.mp3")
