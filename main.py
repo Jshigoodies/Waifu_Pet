4a71f87be5c1d89f2a265e116dbd27192294182d
unknown <jshigoodies@gmail.com>
2022-03-05 15:12:41 -0600

    added audio interaction

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ import voice_audio as va
 
 text = va.get_audio()
 
+print("you said: ", text) # comment out
+
 va.check(text=text)
+
+
--- voice_audio.py
+++ voice_audio.py
@@ -5,6 +5,12 @@ from playsound import playsound
 import speech_recognition as sr
 from gtts import gTTS
 from colors import COLOR
+
+
+# initialize a global arary of strings. Such as greetings. Takes in all the "hello", "hi", "how are you", "howdy",
+# "greetings", "bonjour". 
+
+
 # make a method that checks the input text. Return a string of the audio file back. Use playsound to play the sound.
 
 def get_audio():
@@ -23,6 +29,9 @@ def get_audio():
 
 # check the text that would play the right audio back
 def check(text):
+    if "hello" in text:  # change this later
+        print(COLOR.RED + "Qiqi, I'm a jiangshi. Hm...? What else...")
+        playsound("audio/hello.mp3")
     if "morning" in text:
-        print(COLOR.RED + COLOR.BOLD + "It's morning already? What should I... I'll check my notes.")
+        print(COLOR.RED + "It's morning already? What should I... I'll check my notes.")
         playsound("audio/good_morning.mp3")
