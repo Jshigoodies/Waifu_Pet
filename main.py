6406e3033a5a46b1636539b18ae7fa8ea8fad4f7
unknown <jshigoodies@gmail.com>
2022-03-05 14:16:43 -0600

    added audio

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import os
-import time
-import playsound
-import speech_recognition as sr
-from gtts import gTTS
 
-# make a method that checks the input text. Return a string of the audio file back. Use playsound to play the sound.
+import voice_audio as va
+
+
+text = va.get_audio()
+
+va.check(text=text)
--- voice_audio.py
+++ voice_audio.py
@@ -1,4 +1,28 @@
+import os
+import time
+import playsound
 from playsound import playsound
+import speech_recognition as sr
+from gtts import gTTS
+from colors import COLOR
+# make a method that checks the input text. Return a string of the audio file back. Use playsound to play the sound.
 
-playsound("audio/good_morning.mp3")
-playsound("audio/hello.mp3")
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)  # find the microphone
+        said = ""
+
+        try:
+            said = r.recognize_google(audio)
+        except Exception as e:
+            print("Exception: " + str(e))
+
+    return said
+
+
+# check the text that would play the right audio back
+def check(text):
+    if "morning" in text:
+        print(COLOR.RED + COLOR.BOLD + "It's morning already? What should I... I'll check my notes.")
+        playsound("audio/good_morning.mp3")
