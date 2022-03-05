984db17ab4ecd2de17fb572bbc4c92e976784411
unknown <jshigoodies@gmail.com>
2022-03-05 12:52:19 -0600

    init

---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+import time
+import playsound
+import speech_recognition as sr
+from gtts import gTTS
+
+# make a method that checks the input text. Return a string of the audio file back. Use playsound to play the sound.
---
+++ voice_audio.py
@@ -0,0 +1,4 @@
+from playsound import playsound
+
+playsound("audio/good_morning.mp3")
+playsound("audio/hello.mp3")
