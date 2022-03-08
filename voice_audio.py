c383e52c69473b27f19988ac1fcbcaa1c582b73a
unknown <jshigoodies@gmail.com>
2022-03-07 19:38:22 -0600

    more audio

--- voice_audio.py
+++ voice_audio.py
@@ -7,9 +7,13 @@ from gtts import gTTS
 from colors import COLOR
 
 # initialize a global array of strings. Such as greetings. Takes in all the "hello", "hi", "how are you", "howdy",
+
+# all possible words to trigger a response
+
 hello = ["hello", "greetings", "bonjour", "hi", "hey", "howdy"]  # all the ways to greet someone
 morning = ["morning", "a.m.", "early", "sunrise"]
-
+rain = ["rain", "raining", "rainfall", "precipitation", "raindrops", "rainstorm", "hurricane", "thunder", "storm", "rainwater", "rainy", "flood", "flooding", "hail", "pouring", "umbrella"]
+snow = ["winter", "snow", "snowing", "snowflakes", "snowball", "snowman", "santa"]
 # make a method that checks the input text. Return a string of the audio file back. Use playsound to play the sound.
 
 def get_audio():
@@ -45,7 +49,17 @@ def check(text):
                 talk = True
                 print(COLOR.RED + "It's morning already? What should I... I'll check my notes.")
                 playsound("audio/good_morning.mp3")
+            if word in rain:
+                talk = True
+                print(COLOR.RED + "Forgot an umbrella again.")
+                playsound("audio/rain.mp3")
+            if word in snow:
+                talk = True
+                print(COLOR.RED + "Wanna build a snowman... Can you help?")
+                playsound("audio/snow.mp3")
 
         if not talk:  # did not say anything
             print(COLOR.RED + "What?")
             playsound("audio/nani_ka.mp3")
+
+# a problem i found. If you say the same word 3 times. she will tell the same thing 3 times. Probably need a counter so she doesn't repeat the same sentence over and over again
