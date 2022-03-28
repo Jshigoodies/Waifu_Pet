2525e2cd1591e89e391a7b5453de112d3a151cba
unknown <jshigoodies@gmail.com>
2022-03-28 00:35:11 -0500

    got all the audio files

--- voice_audio.py
+++ voice_audio.py
@@ -12,12 +12,14 @@ from colors import COLOR
 
 hello = ["hello", "greetings", "bonjour", "hi", "hey", "howdy"]  # all the ways to greet someone
 morning = ["morning", "a.m.", "early", "sunrise"]
+afternoon = ["afternoon"]  # ADD THIS #ADD THIS
+evening = ["evening"]  # ADD THIS #ADD THIS
 rain = ["rain", "raining", "rainfall", "precipitation", "raindrops", "rainstorm", "hurricane", "thunder", "storm",
         "rainwater", "rainy", "flood", "flooding", "hail", "pouring", "umbrella"]
 snow = ["winter", "snow", "snowing", "snowflakes", "snowball", "snowman", "santa"]
-cool = ["cold", "wind", "windy", "icy", "ice", "snow", "winter", "snowing",
+cool = ["cold", "wind", "icy", "ice", "snow", "winter", "snowing",
         "frosty"]  # when it's windy "cool, feels nice"
-wind = ["blowing"]  # when the wind is blowing
+wind = ["blowing", "windy"]  # when the wind is blowing
 
 
 # sun is out, afraid of heat
@@ -40,12 +42,16 @@ def get_audio():
     return said
 
 
+def respond(text):  # I need a new fking method
+    pass
+
+
 # check the text that would play the right audio back
 def check(text):
     # have a way to shut her up, maybe make a method for both shut up and un-shut up
     talk = False  # if she said something, then i don't have to make her say nani-ka
 
-    stop = [False, False, False, False, False]
+    stop = [False, False, False, False, False, False]
 
     if "chichi" in text:  # you have to say her name
         text_array = text.split()
@@ -55,33 +61,39 @@ def check(text):
 
             if (word in hello) and (stop[0] == False):  # change this later so get an array of greetings
                 talk = True
-                stop[0] = True # don't run this if statement ever again. Don't make my waifu repeat herself. Is basically what this is doing
+                stop[
+                    0] = True  # don't run this if statement ever again. Don't make my waifu repeat herself. Is basically what this is doing
                 print(COLOR.RED + "Qiqi, I'm a jiangshi. Hm...? What else...")
-                playsound("audio/hello.mp3")
+                playsound("audio/japanese/hello.mp3")
             if (word in morning) and (stop[1] == False):
                 talk = True
                 stop[1] = True
                 print(COLOR.RED + "It's morning already? What should I... I'll check my notes.")
-                playsound("audio/good_morning.mp3")
+                playsound("audio/japanese/good_morning.mp3")
             if (word in rain) and (stop[2] == False):
                 talk = True
                 stop[2] = True
                 print(COLOR.RED + "Forgot an umbrella again.")
-                playsound("audio/rain.mp3")
+                playsound("audio/japanese/rain.mp3")
             if (word in snow) and (stop[3] == False):
                 talk = True
                 stop[3] = True
                 print(COLOR.RED + "Wanna build a snowman... Can you help?")
-                playsound("audio/snow.mp3")
+                playsound("audio/japanese/snow.mp3")
             if (word in cool) and (stop[4] == False):
                 talk = True
                 stop[4] = True
                 print(COLOR.RED + "Cool, feels nice.")
-                playsound("audio/cool.mp3")
+                playsound("audio/japanese/cool.mp3")
+            if (word in wind) and (stop[5] == False):
+                talk = True
+                stop[5] = True
+                print(COLOR.RED + "Hold... hold my hand! I... I'll be blown away!")
+                playsound("audio/japanese/wind.mp3")
 
         if not talk:  # did not say anything
             print(COLOR.RED + "What?")
-            playsound("audio/nani_ka.mp3")
+            playsound("audio/japanese/nani_ka.mp3")
 
 
 # a problem i found. If you say the same word 3 times. she will tell the same thing 3 times. Probably need a counter
