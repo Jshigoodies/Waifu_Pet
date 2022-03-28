a4e35e73a0b1879060a8ed55b8a173b532c49362
unknown <jshigoodies@gmail.com>
2022-03-28 00:54:27 -0500

    scrapped old code to implement a better system

--- voice_audio.py
+++ voice_audio.py
@@ -11,15 +11,25 @@ from colors import COLOR
 # all possible words to trigger a response
 
 hello = ["hello", "greetings", "bonjour", "hi", "hey", "howdy"]  # all the ways to greet someone
-morning = ["morning", "a.m.", "early", "sunrise"]
-afternoon = ["afternoon"]  # ADD THIS #ADD THIS
-evening = ["evening"]  # ADD THIS #ADD THIS
-rain = ["rain", "raining", "rainfall", "precipitation", "raindrops", "rainstorm", "hurricane", "thunder", "storm",
-        "rainwater", "rainy", "flood", "flooding", "hail", "pouring", "umbrella"]
-snow = ["winter", "snow", "snowing", "snowflakes", "snowball", "snowman", "santa"]
-cool = ["cold", "wind", "icy", "ice", "snow", "winter", "snowing",
-        "frosty"]  # when it's windy "cool, feels nice"
-wind = ["blowing", "windy"]  # when the wind is blowing
+chat_talking_to_herself = []
+chat_afraid_of_heat = []
+chat_training = []
+when_it_rains = ["rain", "raining", "rainfall", "precipitation", "raindrops", "rainstorm", "hurricane", "thunder", "storm", "rainwater", "rainy", "flood", "flooding", "hail", "pouring", "umbrella"]
+when_the_sun_is_out = []
+when_its_windy = []
+when_the_wind_is_blowing = ["blowing", "windy"]  # when the wind is blowing
+when_it_snows = ["winter", "snow", "snowing", "snowflakes", "snowball", "snowman", "santa"]
+good_morning = ["morning", "a.m.", "early", "sunrise"]
+good_afternoon = ["afternoon"]  # ADD THIS #ADD THIS
+good_evening = ["evening"]  # ADD THIS #ADD THIS
+good_night = []
+about_qiqi = []
+about_us_memory = []
+about_us_memory_of_training = []
+about_the_vision = []
+something_to_share = []
+interesting_things = []
+about_baizhu = []
 
 
 # sun is out, afraid of heat
@@ -46,61 +56,5 @@ def respond(text):  # I need a new fking method
     pass
 
 
-# check the text that would play the right audio back
-def check(text):
-    # have a way to shut her up, maybe make a method for both shut up and un-shut up
-    talk = False  # if she said something, then i don't have to make her say nani-ka
-
-    stop = [False, False, False, False, False, False]
-
-    if "chichi" in text:  # you have to say her name
-        text_array = text.split()
-
-        for word in text_array:  # check each word probably slow as fk
-            # print(word) # prints out what i said in a list
-
-            if (word in hello) and (stop[0] == False):  # change this later so get an array of greetings
-                talk = True
-                stop[
-                    0] = True  # don't run this if statement ever again. Don't make my waifu repeat herself. Is basically what this is doing
-                print(COLOR.RED + "Qiqi, I'm a jiangshi. Hm...? What else...")
-                playsound("audio/japanese/hello.mp3")
-            if (word in morning) and (stop[1] == False):
-                talk = True
-                stop[1] = True
-                print(COLOR.RED + "It's morning already? What should I... I'll check my notes.")
-                playsound("audio/japanese/good_morning.mp3")
-            if (word in rain) and (stop[2] == False):
-                talk = True
-                stop[2] = True
-                print(COLOR.RED + "Forgot an umbrella again.")
-                playsound("audio/japanese/rain.mp3")
-            if (word in snow) and (stop[3] == False):
-                talk = True
-                stop[3] = True
-                print(COLOR.RED + "Wanna build a snowman... Can you help?")
-                playsound("audio/japanese/snow.mp3")
-            if (word in cool) and (stop[4] == False):
-                talk = True
-                stop[4] = True
-                print(COLOR.RED + "Cool, feels nice.")
-                playsound("audio/japanese/cool.mp3")
-            if (word in wind) and (stop[5] == False):
-                talk = True
-                stop[5] = True
-                print(COLOR.RED + "Hold... hold my hand! I... I'll be blown away!")
-                playsound("audio/japanese/wind.mp3")
-
-        if not talk:  # did not say anything
-            print(COLOR.RED + "What?")
-            playsound("audio/japanese/nani_ka.mp3")
-
-
-# a problem i found. If you say the same word 3 times. she will tell the same thing 3 times. Probably need a counter
-# so she doesn't repeat the same sentence over and over again
-
-def repeat_condition(array, word):
-    if word in array:
-        return True
-    else:
-        return False
+def check_array(text):
+    pass
