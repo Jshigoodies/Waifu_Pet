1f5e30d1519c78cc0a034d90b0de06212c3b90cc
unknown <jshigoodies@gmail.com>
2022-03-29 01:21:28 -0500

    ogg to mp3 and created a system outline for future code

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-
 import voice_audio as va
 from colors import COLOR
+
+# make it so you can ask for her to speak in a specific langauge
 
 while True:
     text = va.get_audio()
     text = text.lower()
-    print(COLOR.RESET + "you said: ", text) # comment out
+    print(COLOR.RESET + "you said: ", text)  # comment out
 
-    va.check(text=text)
-
+    va.respond(text=text)
--- voice_audio.py
+++ voice_audio.py
@@ -11,10 +11,11 @@ from colors import COLOR
 # all possible words to trigger a response
 
 hello = ["hello", "greetings", "bonjour", "hi", "hey", "howdy"]  # all the ways to greet someone
-chat_talking_to_herself = []
+chat_talking_to_herself = []  # i might need to figure out a phrase instead of using one word
 chat_afraid_of_heat = []
 chat_training = []
-when_it_rains = ["rain", "raining", "rainfall", "precipitation", "raindrops", "rainstorm", "hurricane", "thunder", "storm", "rainwater", "rainy", "flood", "flooding", "hail", "pouring", "umbrella"]
+when_it_rains = ["rain", "raining", "rainfall", "precipitation", "raindrops", "rainstorm", "hurricane", "thunder",
+                 "storm", "rainwater", "rainy", "flood", "flooding", "hail", "pouring", "umbrella"]
 when_the_sun_is_out = []
 when_its_windy = []
 when_the_wind_is_blowing = ["blowing", "windy"]  # when the wind is blowing
@@ -30,7 +31,27 @@ about_the_vision = []
 something_to_share = []
 interesting_things = []
 about_baizhu = []
+about_hutao = []
+about_xiao_name = []
+about_xiao_memory = []
+about_xinyan = []
+about_yaoyao = []
+about_dusky_ming = []
+more_about_qiqi_one = []  # file num 1
+more_about_qiqi_two = []  # file num 2
+more_about_qiqi_three = []  # file num 3
+more_about_qiqi_four = []  # file num 4
+more_about_qiqi_five = []  # file num 5
+qiqi_hobbies = []
+qiqi_troubles = []
+favorite_food = []
+least_favorite_food = []
+birthday = []
+feelings_about_ascension_building_up = []
+feelings_about_ascension_climax = []
 
+
+# Only ascension phase 2 and 4 makes sense. The ogg files is labeled 2 and 3.
 
 # sun is out, afraid of heat
 
@@ -53,8 +74,29 @@ def get_audio():
 
 
 def respond(text):  # I need a new fking method
-    pass
+    understand = False
+    stop = [False, False]  # don't repeat
+    text_array = text.split()
+
+    if "chichi" not in text_array:
+        return
+
+    for word in text_array:
+        if check_array(word) and stop[0] == False:
+            understand = True
+            stop[0] = True
+
+    if not understand:
+        print(COLOR.RED + "What's going on?")
+        playsound("audio\english\more_about_qiqi_friendship\VO_Qiqi_More_About_Qiqi_-_01.mp3")
 
 
 def check_array(text):
+    if text in hello:
+        print(COLOR.RED + "I am Qiqi. I am a zombie. And I forgot what comes next.")
+        playsound("audio\english\VO_Qiqi_Hello.mp3")
+        return True
+
+
+def check_good_day_array(text):
     pass
